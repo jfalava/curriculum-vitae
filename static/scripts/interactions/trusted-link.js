d78ea5ad139bdc30ff19d45f7b5c2cc28b240569
Jorge Fernando Álava <contacto@jfalava.eu>
2023-08-31 11:36:56 +0200

    v1.7a

--- static/scripts/interactions/trusted-link.js
+++ static/scripts/interactions/trusted-link.js
@@ -1,10 +1,27 @@
+  // Function to get URL parameters
   function getUrlParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
   }
-  const trustedParam = getUrlParam('trusted');
-  const phoneNumberDiv = document.getElementById('phone-number');
 
+  // Check if the URL contains "?trusted=true"
+  const trustedParam = getUrlParam('trusted');
+
+  // Get the phone number div element
+  const phoneNumberDiv = document.getElementById('phone-number');
+  // Get the trusted email div element
+  const trustedEmailDiv = document.getElementById('trusted-email');
+  // Get the general email div element
+  const generalEmailDiv = document.getElementById('general-email');
+
+  // If "?trusted=true" is present, reveal the trusted email and hide the general email
   if (trustedParam === 'true') {
-    phoneNumberDiv.style.display = '';
+    phoneNumberDiv.style.display = ''; // or your desired style
+    trustedEmailDiv.style.display = ''; // or your desired style
+    generalEmailDiv.style.display = 'none';
+  } else {
+    // If "?trusted=true" is not present, hide the trusted email and reveal the general email
+    phoneNumberDiv.style.display = 'none';
+    trustedEmailDiv.style.display = 'none';
+    generalEmailDiv.style.display = ''; // or your desired style
   }
